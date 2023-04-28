703623115ad5ddb25d165197da447dd374c59a48
myzticx <131876951+myzticx@users.noreply.github.com>
2023-04-28 11:58:18 +0100

    Add files via upload
    work for me and  manmohn upload

---
+++ assertion.js
@@ -0,0 +1,13 @@
+
+let word = "navi"
+function shouter() {
+    // return true
+    // return str.toUpperCase() + "!";
+}
+
+console.assert(shouter !== undefined, "the shouter function needs to be defined")
+var a = 25;
+var b= 25;
+console.assert(a + b === 50, "we need a + b to equal 50");
+
+console.assert("real" === "real", "scream")
